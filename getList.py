49ddb1dbdc4b9c4fe5b08c3356051e35b38a46d0
Wei Deng <greyman@live.cn>
2016-11-23 09:59:51 -0500

    extract company list

--- getList.py
+++ getList.py
@@ -1,24 +1,31 @@
+#!/usr/bin/python
 import urllib2
 import csv
+import sys
 import numpy as np
 
 def getTickers(percent):
     file = open('./input/tickerList.csv', 'w')
     writer = csv.writer(file, delimiter=',')
     capStat, output = np.array([]), []
-    for exchange in ["NASDAQ", "NYSE", "AMEX"]:
-        print "Download tickers from " + exchange
-        u0 = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange="
-        u1 = "&render=download"
-        response = urllib2.urlopen(u0 + exchange + u1)
-        content = response.read().split('\n')
-        for num, line in enumerate(content):
-            line = line.strip().strip('"').split('","')
-            if num == 0 or len(line) != 9: continue # filter unmatched format
-            ticker, name, lastSale, MarketCap, IPOyear, sector, \
-            industry = line[0: 4] + line[5: 8]
-            capStat = np.append(capStat, float(MarketCap))
-            output.append([ticker, name.replace(',', '').replace('.', ''), exchange, MarketCap])
+    for exchange in ["NASDAQ", "NYSE", "AMEX"]:        
+        url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange="
+        repeat_times = 10 # repeat downloading in case of http error
+        for _ in range(repeat_times): 
+            try:
+                print "Download tickers from " + exchange
+                response = urllib2.urlopen(url + exchange + '&render=download')
+                content = response.read().split('\n')
+                for num, line in enumerate(content):
+                    line = line.strip().strip('"').split('","')
+                    if num == 0 or len(line) != 9: continue # filter unmatched format
+                    ticker, name, lastSale, MarketCap, IPOyear, sector, \
+                    industry = line[0: 4] + line[5: 8]
+                    capStat = np.append(capStat, float(MarketCap))
+                    output.append([ticker, name.replace(',', '').replace('.', ''), exchange, MarketCap])
+                break
+            except:
+                continue
 
     for data in output:
         marketCap = float(data[3])
@@ -27,7 +34,8 @@ def getTickers(percent):
 
 
 def main():
-    s = getTickers(1) # keep the top N% market-cap companies
+    arg = sys.argv[1]
+    s = getTickers(int(arg)) # keep the top N% market-cap companies
 
 
 if __name__ == "__main__":
