4eaa5883da19702932532321ba4efa87acbc1cbb
Wei Deng <greyman@live.cn>
2016-11-23 10:00:31 -0500

    crawl news from bloomberg

--- crawler.py
+++ crawler.py
@@ -1,52 +1,70 @@
+#!/usr/bin/python
 import re
 import urllib2
 import csv
-import random
+import os
+import sys
 import time
 
-
-from sets import Set
+import numpy as np
 from bs4 import BeautifulSoup
 
 '''
 Other useful crawler can be found in 
 https://doc.scrapy.org/en/latest/
 github.com/qolina/ScrapyFinanceNews/tree/master/ScrapyNews/spiders
-
 '''
 
 class newsCrawler:
     def __init__(self):
         fin = open('./input/tickerList.csv')
-        fout = open('./input/bloombergNews.txt', 'a+')
-        writer = csv.writer(fout, delimiter=',')
+        # exit if the output already existed
+        if os.path.isfile('./input/bloombergNews.csv'):
+            sys.exit("bloombergNews already existed!")
+
         for line in fin:
             line = line.strip().split(',')
             ticker, name, exchange, MarketCap = line
-            self.content(ticker, writer, line)
-        fout.close()
+            self.content(ticker, line)
+            #break
 
-    def content(self, ticker, writer, line):
+    def content(self, ticker, line):
         url = "http://www.bloomberg.com/search?sort=time:desc&query=" + ticker 
-        for pn in range(1, 1000):
+        for pn in range(1, 500):
             print(line, pn)
-            time.sleep(random.uniform(3, 10))
-            response = urllib2.urlopen(url + "&page=" + str(pn))
-            csv = response.read()
-            soup = BeautifulSoup(csv, "lxml")
-            self.parser(soup, ticker, writer)
+            tag = self.repeatDownload(ticker, line, url, pn) # if page has news, tag 1, otherwise tag 0
+            if not tag: break
+
+    def repeatDownload(self, ticker, line, url, pn): 
+        repeat_times = 4 # repeat downloading in case of http error
+        for _ in range(repeat_times): 
+            try:
+                time.sleep(np.random.poisson(6))
+                response = urllib2.urlopen(url + "&page=" + str(pn))
+                data = response.read()
+                soup = BeautifulSoup(data, "lxml")
+                tag = self.parser(soup, line, ticker)
+                if not tag: return tag # if page has no news, return 0 to change ticker
+                break # skip loop if data fetched
+            except:
+                continue
+        return 1
+
             
 
-    def parser(self, soup, ticker, writer):
+    def parser(self, soup, line, ticker):
         timeSet = soup.find_all("div", class_="search-result-story__metadata")
         titles = soup.find_all("h1", class_="search-result-story__headline")
         tags = soup.find_all("div", class_="search-result-story__body")
-        
-        for i in range(len(timeSet)):
+        fout = open('./input/bloombergNews.csv', 'a+')
+        if len(timeSet) == 0: return 0
+        for i in range(len(timeSet)):    
             timestamp = self.timeConvert(timeSet[i].time.get_text())
             title = " ".join(re.findall(r"\w+", titles[i].a.get_text()))
             tag = " ".join(re.findall(r"\w+", tags[i].get_text()))
-            writer.writerow([ticker, timestamp, title, tag])
+            fout.write(','.join([ticker, line[1], timestamp, title]) + '\n')
+        fout.close()
+        return 1
 
     def timeConvert(self, time):
         conv = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', \
@@ -54,6 +72,7 @@ class newsCrawler:
                 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
         timeFmt = ' '.join(re.findall(r"\w+", time)) # format Nov 22 2016
         periods = timeFmt.split(" ")
+        if len(periods[1]) == 1: periods[1] = '0' + periods[1]
         newTime = ''.join([periods[2], conv[periods[0]], periods[1]]) # format 20161122
         return newTime
     
