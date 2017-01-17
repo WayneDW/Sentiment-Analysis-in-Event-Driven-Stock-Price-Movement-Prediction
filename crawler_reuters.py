6dff0aa50858073863f3d556d2bac1e956d9ef42
waynedw <greyman@live.cn>
2017-01-16 20:30:47 -0500

    update top story

--- crawler_reuters.py
+++ crawler_reuters.py
@@ -35,25 +35,45 @@ class news_Reuters:
             line = line.strip().split(',')
             ticker, name, exchange, MarketCap = line
             if ticker in filterList: continue
-            self.content(ticker, line, dateList)
+            self.content(ticker, name, line, dateList)
 
-    def content(self, ticker, line, dateList):
-        # http://www.reuters.com/finance/stocks/companyNews?symbol=GOOGL.O&date=11162016
-        url = "http://www.reuters.com/finance/stocks/companyNews?symbol=" + ticker 
-        tag = 0
-        for timestamp in dateList:
-            print(line, timestamp)
-            hasNews = self.repeatDownload(ticker, line, url, timestamp) 
-            if hasNews: tag = 0 # if get news, reset tag as 0
-            else: tag += 1
-            if tag > 30: break # if we can't receive news in 30 consecutive days, stop this ticker
+    def content(self, ticker, name, line, dateList):
+        url = "http://www.reuters.com/finance/stocks/companyNews?symbol=" + ticker
+
+        # some company even doesn't have a single news, stop bothering iterating dates if so
+        has_Content = 0
+        repeat_times = 4
+        for _ in range(repeat_times): # in case of crawling failure
+            try:
+                time.sleep(np.random.poisson(3))
+                response = urllib2.urlopen(url)
+                data = response.read()
+                soup = BeautifulSoup(data, "lxml")
+                has_Content = len(soup.find_all("div", {'class': ['topStory', 'feature']}))
+                break
+            except:
+                continue
+        
+        if has_Content > 0:
+            missing_days = 0
+            print(ticker, name)
+            for timestamp in dateList:
+                hasNews = self.repeatDownload(ticker, line, url, timestamp) 
+                if hasNews: missing_days = 0 # if get news, reset missing_days as 0
+                else: missing_days += 1
+                if missing_days > has_Content * 5 + 20: # 2 NEWS: wait 30 days and stop, 10 news, wait 70 days
+                    break # no news in X consecutive days, stop crawling
+                if missing_days > 0 and missing_days % 20 == 0: # print the process
+                    print(ticker, "has no news for ", missing_days, " days")
+        else:
+            print(ticker, "has no single news")
 
     def repeatDownload(self, ticker, line, url, timestamp): 
         new_time = timestamp[4:] + timestamp[:4] # change 20151231 to 12312015 to satisfy reuters format
-        repeat_times = 4 # repeat downloading in case of http error
+        repeat_times = 3 # repeat downloading in case of http error
         for _ in range(repeat_times): 
             try:
-                time.sleep(np.random.poisson(2))
+                time.sleep(np.random.poisson(3))
                 response = urllib2.urlopen(url + "&date=" + new_time)
                 data = response.read()
                 soup = BeautifulSoup(data, "lxml")
@@ -65,14 +85,18 @@ class news_Reuters:
         return 0
   
     def parser(self, soup, line, ticker, timestamp):
-        content = soup.find_all("div", class_="feature")
-        fout = open('./input/news_reuters_part5.csv', 'a+')
-        
+        content = soup.find_all("div", {'class': ['topStory', 'feature']})
         if len(content) == 0: return 0
-        for i in range(len(content)):    
-            title = ' '.join(re.findall(r"\w+", content[i].a.get_text()))
-            body = ' '.join(re.findall(r"\w+", content[i].p.get_text()))
-            fout.write(','.join([ticker, line[1], timestamp, title, body]) + '\n')
+        fout = open('./input/news_reuters.csv', 'a+')
+        for i in range(len(content)):
+            title = content[i].h2.get_text().replace(",", " ").replace("\n", " ")
+            body = content[i].p.get_text().replace(",", " ").replace("\n", " ")
+
+            if i == 0 and len(soup.find_all("div", class_="topStory")) > 0: news_type = 'topStory'
+            else: news_type = 'normal'
+
+            print(ticker, timestamp, title, news_type)
+            fout.write(','.join([ticker, line[1], timestamp, title, body, news_type]) + '\n')
         fout.close()
         return 1
     
@@ -84,8 +108,6 @@ class news_Reuters:
 
 def main():
     news_Reuters()
-    
-
 
 if __name__ == "__main__":
     main()
