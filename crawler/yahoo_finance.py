5458a6505d2777cece0fe242c4ae0e804a7d3bfc
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-20 22:15:47 -0400

    use yqd.load_yahoo_quote in yahoo_finance.py

--- crawler/yahoo_finance.py
+++ crawler/yahoo_finance.py
@@ -1,28 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+"""
+Crawl daily price data from yahoo finance to generate raw data
+
+Require "./input/news_reuters.csv"
+==> "./input/finished.reuters" by calc_finished_ticker()
+==> "./input/stockPrices_raw.json" by get get_stock_prices()
+json structure:
+         ticker
+        /  |   \
+    open close adjust ...
+      /    |     \
+   dates dates  dates ...
+"""
 import sys
-import urllib2
 import re
 import os
 import time
-import urllib
-import urllib2
-
 import random
 import json
-from math import log
 
-# output file name: input/stockPrices_raw.json
-# json structure: crawl daily price data from yahoo finance
-#          ticker
-#         /  |   \       
-#     open close adjust ...
-#       /    |     \
-#    dates dates  dates ...
+# Credit: https://github.com/c0redumb/yahoo_quote_download/blob/master/yahoo_quote_download/yqd.py
+from yqd import load_yahoo_quote
+
 
 def calc_finished_ticker():
     os.system("awk -F',' '{print $1}' ./input/news_reuters.csv | sort | uniq > ./input/finished.reuters")
 
-def get_stock_Prices():
+
+def get_stock_prices():
     fin = open('./input/finished.reuters')
     output = './input/stockPrices_raw.json'
 
@@ -30,61 +35,56 @@ def get_stock_Prices():
     if os.path.isfile(output):
         sys.exit("Prices data already existed!")
 
-    priceSet = {}
-    priceSet['^GSPC'] = repeatDownload('^GSPC') # download S&P 500
+    price_set = {}
+    price_set['^GSPC'] = repeat_download('^GSPC') # download S&P 500
     for num, line in enumerate(fin):
         ticker = line.strip()
         print(num, ticker)
-        priceSet[ticker] = repeatDownload(ticker)
-        # if num > 10: break # for testing purpose
+        price_set[ticker] = repeat_download(ticker)
+        # if num >= 10: break # for testing purpose
 
     with open(output, 'w') as outfile:
-        json.dump(priceSet, outfile, indent=4)
+        json.dump(price_set, outfile, indent=4)
 
 
-def repeatDownload(ticker):
+def repeat_download(ticker):
     repeat_times = 3 # repeat download for N times
-    for _ in range(repeat_times): 
+    for i in range(repeat_times):
         try:
             time.sleep(random.uniform(2, 3))
-            priceStr = PRICE(ticker)
-            if len(priceStr) > 0: # skip loop if data is not empty
-                break
-        except:
-            if _ == 0: print ticker, "Http error!"
-    return priceStr
+            price_str = get_price_from_yahoo(ticker)
+            if price_str: # skip loop if data is not empty
+                return price_str
+        except Exception as e:
+            print(e)
+            if i == 0:
+                print(ticker, "Http error!")
 
+def get_price_from_yahoo(ticker):
 
-def PRICE(ticker):
-    start_y, start_m, start_d = '2004', '01', '01' # starting date
-    end_y, end_m, end_d = '2999', '12', '01' # until now
+    start_date = '20040101'
+    end_date = '29991201'  # until now
+    quote = load_yahoo_quote(ticker, start_date, end_date)
 
-    # Construct url
-    url1 = "http://chart.finance.yahoo.com/table.csv?s=" + ticker
-    url2 = "&a=" + start_m + "&b=" + start_d + "&c=" + start_y
-    url3 = "&d=" + end_m + "&e=" + end_d + "&f=" + end_y + "&g=d&ignore=.csv"
-
-    # parse url
-    response = urllib2.urlopen(url1 + url2 + url3)
-    csv = response.read().split('\n')
     # get historical price
     ticker_price = {}
-    index = ['open', 'high', 'low', 'close', 'volume', 'adjClose']
-    for num, line in enumerate(csv):
+    index = ['open', 'high', 'low', 'close', 'adjClose', 'volume']
+    for num, line in enumerate(quote):
         line = line.strip().split(',')
-        if len(line) < 7 or num == 0: continue
+        if len(line) < 7 or num == 0:
+            continue
         date = line[0]
         # check if the date type matched with the standard type
-        if not re.search(r'^[12]\d{3}-[01]\d-[0123]\d$', date): continue
+        if not re.search(r'^[12]\d{3}-[01]\d-[0123]\d$', date):
+            continue
         # open, high, low, close, volume, adjClose : 1,2,3,4,5,6
-        for num, typeName in enumerate(index):
+        for num, type_name in enumerate(index, 1):
             try:
-                ticker_price[typeName][date] = round(float(line[num + 1]),2)
+                ticker_price[type_name][date] = round(float(line[num]), 2)
             except:
-                ticker_price[typeName] = {}
+                ticker_price[type_name] = {}
     return ticker_price
 
-
 if __name__ == "__main__":
-    calc_finished_ticker()
-    get_stock_Prices()
+    # calc_finished_ticker()
+    get_stock_prices()
