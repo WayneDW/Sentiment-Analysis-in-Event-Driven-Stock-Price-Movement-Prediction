54ed71682a74cf26629371da6289394185a94547
waynedw <greyman@live.cn>
2018-05-31 23:27:36 -0400

    not to confuse run_web_server

--- run_model.sh
+++ run_model.sh
@@ -7,6 +7,6 @@ target_date=`date -d '-1 day' '+%Y%m%d'`
 ./crawler/daily_reuters.py
 
 #./main.py -vocabs ${vocabs} -epochs 500 -static False
-./main.py -vocabs ${vocabs} -eval True
-./main.py -vocabs ${vocabs} -predict "Top executive behind Baidu's artificial intelligence drive steps aside"
+#./main.py -vocabs ${vocabs} -eval True
+#./main.py -vocabs ${vocabs} -predict "Top executive behind Baidu's artificial intelligence drive steps aside"
 ./main.py -vocabs ${vocabs} -predict True -date ${target_date}
